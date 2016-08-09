9ba8d814487c6d2bac2ded6dc757d95e08b80a34
francisco2016 <otrayama@gmail.com>
2016-08-09 23:07:28 +0200

    - 0692 clase Snake, añadido mt colisionConBordes(), colisionConSegmento() y segmentoValido().

--- Snake.java
+++ Snake.java
@@ -1,33 +1,41 @@
-
+import java.util.ArrayList;
 /**
- * Write a description of class Snake here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @Francisco
  */
-public class Snake
-{
-    // instance variables - replace the example below with your own
-    private int x;
+class Snake {
+    private ArrayList<Segment> segmentos;
+    public static final int LARGO_LIENZO = 500;
+    public static final int ALTO_LIENZO = 500;
 
-    /**
-     * Constructor for objects of class Snake
-     */
-    public Snake()
-    {
-        // initialise instance variables
-        x = 0;
+    public Snake() {
+        segmentos = new ArrayList<>();
     }
-
+    
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * devuelve true si el segmento colisiona con los bordes.
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public boolean colisionBordes(Segment segmento){
+        return ((segmento.getPosiX() == 0) || (segmento.getPosiX() == LARGO_LIENZO)) ||
+                ((segmento.getPosiY() == 0) || (segmento.getPosiY() == ALTO_LIENZO));              
+    }
+  
+    /**
+     * devuelve si el segmento coliona con otros segmentos.
+     */
+    public boolean colisionConSegmentos(Segment segmento){
+        boolean colision = false;
+        for(Segment segment: segmentos){
+            if(segment.colisionaCon(segmento)){
+               colision = true;
+            }
+        }
+        return colision;
+    }
+    
+    /**
+     * devuelve true si el segmento es válido para añadirse.
+     */
+    public boolean segmentValido(Segment segmento){
+        return !colisionBordes(segmento) && !colisionBordes(segmento);
     }
 }
