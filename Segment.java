6e722a10fb6e773c467e3f6138a47f96a1f362bf
francisco2016 <otrayama@gmail.com>
2016-08-09 16:43:46 +0200

    - 0692 add atributos y constructor en la clase Segment.

--- Segment.java
+++ Segment.java
@@ -1,33 +1,32 @@
+import java.awt.Color;
+import java.util.Random;
 
 /**
- * Write a description of class Segment here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ *
+ * @author Usuario
  */
-public class Segment
-{
-    // instance variables - replace the example below with your own
-    private int x;
+class Segment {
+    //atributos para la clase.
+    private int posiX;
+    private int posiY;
+    public static final int LONG_SEGMENTO = 15;
+    private int direccion;
+    private Color color;
+    
+    private Random aleatorio;
+    
+    //Constructor que inicializa para inicializar atributos.
 
-    /**
-     * Constructor for objects of class Segment
-     */
-    public Segment()
-    {
-        // initialise instance variables
-        x = 0;
+    public Segment(int posiX, int posiY, int direccion) {
+        this.posiX = posiX;
+        this.posiY = posiY;
+        this.direccion = (direccion != 0 && direccion != 90 && 
+                direccion != 180 && direccion != 270 ) ? 180 : direccion;
+        aleatorio = new Random();
+        color = new Color(aleatorio.nextInt(225), aleatorio.nextInt(225),
+                            aleatorio.nextInt(225));
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
+    
+    
 }
